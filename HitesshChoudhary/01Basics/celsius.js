bad8afe6c916a5e95031a138a3862b07513b7f0a
Prabhu Narayanan <nrprabhu@users.noreply.github.com>
2018-12-14 19:45:53 -0800

    Create javascript repo and add 3 js chapter code

---
+++ HitesshChoudhary/01Basics/celsius.js
@@ -0,0 +1,6 @@
+let tempInFahrenheit = 100;
+//T(°C) = (T(°F) - 32) × 5/9
+
+let tempInCelsius = (tempInFahrenheit -32) * (5/9);
+console.log(tempInCelsius);
+
---
+++ HitesshChoudhary/01Basics/variable.js
@@ -0,0 +1,18 @@
+let name = 'john';
+let score = 100;
+let bonus = 20;
+
+let totalScore= score + bonus;
+
+let firstName = 'John';
+    firstName = 'Jane';
+
+let lastName = 'Doe';
+
+let fullName = lastName + ', ' + firstName;
+
+console.log(name);
+console.log(totalScore);
+console.log(fullName);
+
+
