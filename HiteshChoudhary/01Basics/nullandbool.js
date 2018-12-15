7b407832192a9e5e306f68d5e3e28abb3c6ee196
Prabhu Narayanan <nrprabhu@users.noreply.github.com>
2018-12-14 20:06:15 -0800

    added chapter on  null / undefined and  boolean

---
+++ HiteshChoudhary/01Basics/nullandbool.js
@@ -0,0 +1,16 @@
+// Variable created but not initalized
+var temp;
+
+// variable has undefined which is same as null
+console.log(temp);
+
+
+// Boolean
+
+var grade = true
+
+//print boolean value
+console.log(grade);
+
+//print ! value of boolean variable
+console.log(!grade);
